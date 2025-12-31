095063ba5272e49179b27baec3db8424661aa9fc
Mahdi Touiserkani <mahditouserkani@yahoo.com>
2025-12-31 06:57:18 +0330

    Impelemnt the header and top of the application #50

--- apps/landing/src/app/page.tsx
+++ apps/landing/src/app/page.tsx
@@ -4,10 +4,13 @@ import './globals.css';
 import { Box } from '@mui/material';
 
 import AboutSection from './components/AboutSection';
+import Banner from './components/Banner';
 import ContactSection from './components/ContactSection';
 import FeaturesSection from './components/FeaturesSection';
 import Footer from './components/Footer';
+import Header from './components/Header';
 import HeroBanner from './components/HeroBanner';
+import Navbar from './components/Navbar';
 import NewsSection from './components/NewsSection';
 import PricesSection from './components/PricesSection';
 import ThemeProvider from './components/ThemeProvider';
@@ -17,6 +20,9 @@ export default function Page() {
   return (
     <ThemeProvider>
       <Box>
+        <Header />
+        <Navbar />
+        <Banner />
         <HeroBanner />
         <PricesSection />
         <FeaturesSection />
